85365001fac75441c680efa3567859dc0d636ea6
Abid Kodakkadan <abid@rariti.io>
2024-03-20 01:32:34 -0700

    Clean build

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg,
+};
+
+// entrypoint declaration
+entrypoint!(process_instruction);
+
+// entrypoint impl.
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8]
+) -> ProgramResult {
+    // test message log
+    msg!("Success!");
+    // Close the program - needed at the end of all progs.
+    Ok(())
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
